81d6f7301da290f3647f3d23038c4d3a6fd3e350
findchow <findchow.uk@gmail.com>
2025-05-26 00:28:16 +0100

    removed validator

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,15 +1,12 @@
 import { Router } from 'express';
 import { login, me, register, registerReader, signin } from '../controller/auth/controller';
-import { validate } from '../validation';
-import { userInputSchema } from '../validation/business.validator';
-import { loginInputSchema } from '../validation/user.validator';
 
 const router = Router();
 
-router.post('/auth/business/register', validate(userInputSchema) as any, register);
-router.post('/auth/business/login', validate(loginInputSchema) as any, login);
+router.post('/auth/business/register', register);
+router.post('/auth/business/login', login);
 router.get('/auth/me', me);
-router.post("/auth/user/register", validate(loginInputSchema) as any, registerReader);
-router.post("/auth/user/login", validate(loginInputSchema) as any, signin);
+router.post("/auth/user/register", registerReader);
+router.post("/auth/user/login", signin);
 
 export { router as AuthRoutes };
